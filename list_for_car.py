9cda531a643fdcb84b2bc69fea6caba26ea9539c
ErfanGhanizadeh <eghanizadeh4@gmail.com>
2025-09-23 12:15:29 -0700

    Update list_for_car.py

--- list_for_car.py
+++ list_for_car.py
@@ -11,8 +11,9 @@ while True:
           "3)Enter your car name:\n"
           "4)Enter your car color:\n"
           "5)Enter your car since:\n"
-          "6)show list:\n"
-          "7)exit:\n")
+          "6)Show list:\n"
+          "7)Remove:\n"
+          "8)Exit:\n")
 
     options = int(input("select your number from box:"))
     if options == 1:
@@ -34,6 +35,7 @@ while True:
     elif options == 5:
         car_since = input("Enter your car since: ")
         list_car_since.append(car_since)
+
 
     elif options == 6:
         if list_name:
@@ -67,19 +69,68 @@ while True:
         if list_car_since:
             print("=" * 50)
             print("list of car since is :", list_car_since)
+            print("=" * 50)
         else:
             print("=" * 50)
             print("the list of car since is empty!!!")
+            print("=" * 50)
 
 
+    elif options==7:
+        print("which list do you want to remove???\n"
+              "1)list of the name:\n"
+              "2)list of the family:\n"
+              "3)list of the car name:\n"
+              "4)list of the car color:\n"
+              "5)list of the car since:\n")
+        choose=int(input("please enter your number:"))
 
-    if options == 7:
+        if choose==1:
+            if list_name:
+                print("current list is :",list_name)
+                print("="*50)
+                remove=input("please enter which item do you want to remove:")
+                list_name.remove(remove)
+            else:
+                print("current list is empty!!!")
+
+        elif choose==2:
+            if list_family:
+                print("current list is :",list_family)
+                print("="*50)
+                remove=input("please enter which item do you want to remove:")
+                list_family.remove(remove)
+            else:
+                print("current list is empty!!!")
+
+        elif choose==3:
+            if list_car_name:
+                print("current list is :",list_car_name)
+                print("="*50)
+                remove=input("please enter which item do you want to remove:")
+                list_car_name.remove(remove)
+            else:
+                print("current list is empty!!!")
+
+        elif choose==4:
+            if list_car_color:
+                print("current list is :",list_car_color)
+                print("="*50)
+                remove=input("please enter which item do you want to remove:")
+                list_car_color.remove(remove)
+            else:
+                print("current list is empty!!!")
+
+        elif choose==5:
+            if list_car_since:
+                print("current list is :",list_car_since)
+                print("="*50)
+                remove=input("please enter which item do you want to remove:")
+                list_car_since.remove(remove)
+            else:
+                print("current list is empty!!!")
+
+
+    if options == 8:
         print("good buy!!!")
         break
-
-
-    ask1 = input("do you want to continue?(yes,no):")
-    if ask1 == "no":
-        break
-    elif ask1 == "yes":
-        continue
